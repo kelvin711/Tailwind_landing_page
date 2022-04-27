400b22d9ccb0bb0eb27dec15ebfacea41eac2aca
kelvin711 <arauzkelvin99@gmail.com>
2022-04-27 18:48:35 -0500

    hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ module.exports = {
     content: ["./*.html"],
     theme: {
         screens: {
-        sm: "480px",
-        md: "768px",
-        lg: "976px",
-        xl: "1440px"       
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',       
         },
         extend: {
             colors: {
